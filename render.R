a7ecde911877a09c0bd3aff4d89e6e09be18b6e6
GiuseppeTT <67562667+GiuseppeTT@users.noreply.github.com>
2021-05-18 16:11:53 +0000

    Deploying to gh-pages from @ GiuseppeTT/R-Project-Structure@157c6fb169ebdba14ccbc0cfc2554bd32dbef48f 🚀

--- render.R
+++
@@ -1,7 +0,0 @@
-rmarkdown::render(
-    input = here::here("Rmd/presentation.Rmd"),
-    output_file = here::here("output/presentation.html"),
-    intermediates_dir = tempdir(),
-    knit_root_dir = here::here(),
-    clean = TRUE
-)
