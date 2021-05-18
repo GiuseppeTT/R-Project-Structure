8f190ac465f6de82a2f81bfecaa485ad1804ca9e
Giuseppe Tinti Tomio <67562667+GiuseppeTT@users.noreply.github.com>
2021-05-18 02:14:45 -0300

    Add deploy workflow (#4)

--- render.R
+++ render.R
@@ -1,6 +1,6 @@
 rmarkdown::render(
     input = here::here("Rmd/presentation.Rmd"),
-    output_file = here::here("output/presentation.html"),
+    output_file = here::here("output/github-pages/presentation.html"),
     intermediates_dir = tempdir(),
     knit_root_dir = here::here(),
     clean = TRUE
